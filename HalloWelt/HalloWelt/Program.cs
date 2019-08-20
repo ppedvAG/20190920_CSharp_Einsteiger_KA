07ed02d0ac6787afff8d91ed88f01485657c5907
JoeVice <DanielWill@users.noreply.github.com>
2019-08-20 11:50:31 +0200

    Datentypen Ausgabe von Strings, Konvertierung

--- HalloWelt/HalloWelt/Program.cs
+++ HalloWelt/HalloWelt/Program.cs
@@ -10,7 +10,57 @@ namespace HalloWelt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo Welt");
+            #region Ausgabe von Strings
+            //string Name = "Sebastian";
+            //string NachName = "Herb";
+
+            //Console.WriteLine("Hallo " + Name + " " + NachName);
+
+            //Console.WriteLine("Hallo {0} {1}", Name, NachName);
+
+            //Console.WriteLine($"Hallo {Name} {NachName}");
+            #endregion
+
+            #region Konvertierung
+            Console.WriteLine("Zahl eingeben: ");
+            string eingabe = Console.ReadLine();
+
+            int parsed;
+            bool parse = int.TryParse(eingabe, out parsed);
+
+            if (parse)
+            {
+                Console.WriteLine($"Zahl wurde eingegeben: {parsed}");
+            }
+            else
+            {
+                Console.WriteLine($"Keine Zahl! ... Wert: {parsed}");
+            }
+            //Convert.ToDouble();
+            //double.TryParse();
+            //double.Parse();
+            int a = 0;
+            Console.WriteLine(++a);
+
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+
+            a++;
+            Console.WriteLine($"{a}");
+
+            double b = 4;
+
+            // a = b;
+            a = (int)b;
+            a = Convert.ToInt32(b);
+
+
+
+
+
+            #endregion
+
+            Console.WriteLine("--- ENDE PROGRAMM ---");
             Console.ReadLine();
         }
     }
